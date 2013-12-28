ea944532684908077a29c2408e59a1bf4e089a47
Benjamin Dicken <bddicken@gmail.com>
2013-12-27 19:07:14 -0700

    script

---
+++ install/install.sh
@@ -0,0 +1,10 @@
+#~/bin/bash
+
+#
+# Change system to use .*rc files from this repo
+#
+
+echo "source ~/rc/.profile" > ~/.profile
+echo "source ~/rc/.bashrc" > ~/.bashrc
+rm -rf ~/.vimrc
+ln -s ~/rc/.vimrc ~/.vimrc
