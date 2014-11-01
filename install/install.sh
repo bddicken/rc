86e87e1655a5828866ad88484a2d0933c33932ea
bddicken <bddicken@gmail.com>
2014-11-01 09:53:18 -0700

    add git config file

--- install/install.sh
+++ install/install.sh
@@ -4,7 +4,8 @@
 # Change system to use .*rc files from this repo
 #
 
-rm -rf ~/.vimrc ~/.profile ~/.bashrc
+rm -rf ~/.vimrc ~/.profile ~/.bashrc ~/.gitconfig
+ln -s ~/rc/.gitconfig ~/.gitconfig
 ln -s ~/rc/.profile ~/.profile
 ln -s ~/rc/.bashrc ~/.bashrc
 ln -s ~/rc/.vimrc ~/.vimrc
